611daf54c8ab4ff5f674363f530906d640a5383b
Raul <rplasc.2025@gmail.com>
2024-04-21 13:37:20 -0700

    Added crop feature and scaling to image.

--- image-processor/image-processor.pro
+++ image-processor/image-processor.pro
@@ -1,4 +1,4 @@
-QT += core gui opengl widgets
+QT += core gui widgets
 
 LIBS += -lopengl32
 
--- image-processor/main.cpp
+++ image-processor/main.cpp
@@ -2,25 +2,94 @@
 #include <QFileDialog>
 #include <QImageReader>
 
-class GLWidget : public QLabel
+class CropDialog : public QDialog
 {
 public:
-    GLWidget(QWidget *parent = nullptr)
+    CropDialog(QWidget *parent = nullptr)
+        : QDialog(parent)
+    {
+        setWindowTitle("Crop Image");
+
+        QLabel *label = new QLabel("Select crop area:", this);
+        label->setAlignment(Qt::AlignCenter);
+
+        xSpinBox = new QSpinBox(this);
+        xSpinBox->setMinimum(0);
+
+        ySpinBox = new QSpinBox(this);
+        ySpinBox->setMinimum(0);
+
+        widthSpinBox = new QSpinBox(this);
+        widthSpinBox->setMinimum(1);
+
+        heightSpinBox = new QSpinBox(this);
+        heightSpinBox->setMinimum(1);
+
+        QPushButton *okButton = new QPushButton("OK", this);
+        connect(okButton, &QPushButton::clicked, this, &CropDialog::accept);
+
+        QGridLayout *layout = new QGridLayout(this);
+        layout->addWidget(label, 0, 0, 1, 2);
+        layout->addWidget(new QLabel("X:"), 1, 0);
+        layout->addWidget(xSpinBox, 1, 1);
+        layout->addWidget(new QLabel("Y:"), 2, 0);
+        layout->addWidget(ySpinBox, 2, 1);
+        layout->addWidget(new QLabel("Width:"), 3, 0);
+        layout->addWidget(widthSpinBox, 3, 1);
+        layout->addWidget(new QLabel("Height:"), 4, 0);
+        layout->addWidget(heightSpinBox, 4, 1);
+        layout->addWidget(okButton, 5, 0, 1, 2);
+
+        setLayout(layout);
+    }
+
+    QRect getCropRect() const
+    {
+        int x = xSpinBox->value();
+        int y = ySpinBox->value();
+        int width = widthSpinBox->value();
+        int height = heightSpinBox->value();
+
+        return QRect(x, y, width, height);
+    }
+
+private:
+    QSpinBox *xSpinBox;
+    QSpinBox *ySpinBox;
+    QSpinBox *widthSpinBox;
+    QSpinBox *heightSpinBox;
+};
+
+class Widget : public QLabel
+{
+public:
+    Widget(QWidget *parent = nullptr)
         : QLabel(parent)
     {
         setAlignment(Qt::AlignCenter);
+        setScaledContents(true);
     }
 
     void loadImage(const QString &fileName)
     {
-        QPixmap pixmap(fileName);
-        if (pixmap.isNull()) {
+        image.load(fileName);
+        if (image.isNull()) {
             setText("Failed to load image");
             return;
         }
 
-        setPixmap(pixmap.scaled(size(), Qt::KeepAspectRatio));
+        setPixmap(QPixmap::fromImage(image));
     }
+
+    void cropImage(const QRect &rect)
+    {
+        QPixmap cropped = pixmap().copy(rect);
+        setPixmap(cropped.scaled(size(), Qt::KeepAspectRatio));
+        image = cropped.toImage();
+    }
+
+private:
+    QImage image;
 };
 
 class MainWindow : public QMainWindow
@@ -29,12 +98,16 @@ public:
     MainWindow(QWidget *parent = nullptr)
         : QMainWindow(parent)
     {
-        glWidget = new GLWidget(this);
-        setCentralWidget(glWidget);
+        widget = new Widget(this);
+        setCentralWidget(widget);
 
         QAction *openAction = new QAction(tr("&Open..."), this);
         connect(openAction, &QAction::triggered, this, &MainWindow::open);
         menuBar()->addAction(openAction);
+
+        QAction *cropAction = new QAction(tr("&Crop"), this);
+        connect(cropAction, &QAction::triggered, this, &MainWindow::crop);
+        menuBar()->addAction(cropAction);
     }
 
 private slots:
@@ -42,13 +115,22 @@ private slots:
     {
         QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), QString(), tr("Images (*.png *.jpg *.bmp)"));
         if (!fileName.isEmpty()) {
-            glWidget->loadImage(fileName);
+            widget->loadImage(fileName);
             setWindowTitle(fileName);
         }
     }
 
+    void crop()
+    {
+        CropDialog dialog(this);
+        if (dialog.exec() == QDialog::Accepted) {
+            QRect cropRect = dialog.getCropRect();
+            widget->cropImage(cropRect);
+        }
+    }
+
 private:
-    GLWidget *glWidget;
+    Widget *widget;
 };
 
 int main(int argc, char *argv[])
